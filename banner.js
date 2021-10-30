1896953a3dc226940d19c9c21ee0c7397e6563bd
Jorge Cruz <j.alexcruz@gmail.com>
2021-10-30 14:42:28 -0300

    add some action

--- banner.js
+++ banner.js
@@ -1,2 +1,30 @@
 
+// custom your message on banner
+let lgpdHtml = `
+    <div class="lgpd">
+        <div class="lgpd--left">
+                Nós utilozamos cookie para melhor sua experiencia do usuário.<br>
+                Para conferir detalhamente todos os cookies utilizados, leia nossa <a herf="#">politica de privacidade</a>.
+        </div>
+        <div class="lgpd--right">
+                <button> OK! CONCORDAR. </button>
+        </div>
+        </div>
+        <link rel="stylesheet" href="lgpd.css" />
+`;
 
+//
+let lsContent = localStorage.getItem('lgpd');
+
+if(!lsContent) {
+    document.body.innerHTML += lgpdHtml;
+
+    let lgpdArea = document.querySelector('.lgpd');
+    let lgpdButton = lgpdArea.querySelector('button');
+
+    // add click action
+    lgpdButton.addEventListener('click', () => {
+        lgpdArea.remove();  // remove visual banner
+        localStorage.setItem('lgpd', '123'); // fill localstore
+    });
+}
